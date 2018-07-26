d03a50948a24e2d98c5bbcc8a610f36327b2f144
zephyrus <sergey.kilipko@gmail.com>
2018-07-26 14:57:37 +0300

    setup eslint

--- test/functional.test.js
+++ test/functional.test.js
@@ -1,49 +1,44 @@
-var expect = require('chai').expect,
-    Chance = require('chance'),
-    chance = new Chance(),
-    zclId = require('zigbee-bridge-definitions');
+const expect = require('chai').expect;
+const Chance = require('chance');
+const chance = new Chance();
+const zclId = require('zigbee-bridge-definitions');
 
-var zclmeta = require('../lib/zclmeta'),
-    FuncClass = require('../lib/functional');
+const zclmeta = require('../lib/zclmeta');
+const FuncClass = require('../lib/functional');
 
-var clusterIds = [],
-    k;
-
-for (k in zclId._common.clusterId) {
-    clusterIds.push(k);
-}
+const clusterIds = Object.keys(zclId._common.clusterId);
 
 describe('Functional Cmd framer and parser Check', function() {
-    clusterIds.forEach(function (cluster) {
-        var cInfo = zclId._getCluster(cluster),
-            cmdIds = [];
+    clusterIds.forEach(function(cluster) {
+        const cInfo = zclId._getCluster(cluster);
+        const cmdIds = [];
 
         if (!cInfo || !cInfo.cmd) return;
 
-        cInfo.cmd.enums.forEach(function (cmdObj) {
+        cInfo.cmd.enums.forEach(function(cmdObj) {
             cmdIds.push(cmdObj.key);
         });
 
-        cmdIds.forEach(function (cmd) {
-            var funcObj,
-                reqParams,
-                payload,
-                args = {};
+        cmdIds.forEach(function(cmd) {
+            let funcObj;
+            let reqParams;
+            let payload;
+            let args = {};
 
             reqParams = zclmeta.functional.getParams(cluster, cmd);
 
             if (!reqParams) return;
 
-            reqParams.forEach(function (arg) {
+            reqParams.forEach(function(arg) {
                 args[arg.name] = randomArg(arg.type);
             });
 
             funcObj = new FuncClass(cluster, 0, cmd);
             payload = funcObj.frame(args);
 
-            funcObj.parse(payload, function (err, result) {
-                it(funcObj.cmd + ' frame() and parse() check', function () {
-                        expect(result).to.eql(args);
+            funcObj.parse(payload, function(err, result) {
+                it(funcObj.cmd + ' frame() and parse() check', function() {
+                    expect(result).to.eql(args);
                 });
             });
         });
@@ -51,35 +46,35 @@ describe('Functional Cmd framer and parser Check', function() {
 });
 
 describe('Functional CmdRsp framer and parser Check', function() {
-    clusterIds.forEach(function (cluster) {
-        var cInfo = zclId._getCluster(cluster),
-            cmdRspIds = [];
+    clusterIds.forEach(function(cluster) {
+        let cInfo = zclId._getCluster(cluster);
+        let cmdRspIds = [];
 
         if (!cInfo || !cInfo.cmdRsp) return;
 
-        cInfo.cmdRsp.enums.forEach(function (cmdObj) {
+        cInfo.cmdRsp.enums.forEach(function(cmdObj) {
             cmdRspIds.push(cmdObj.key);
         });
 
-        cmdRspIds.forEach(function (cmdRsp) {
-            var funcObj,
-                reqParams,
-                payload,
-                args = {};
+        cmdRspIds.forEach(function(cmdRsp) {
+            let funcObj;
+            let reqParams;
+            let payload;
+            let args = {};
 
             reqParams = zclmeta.functional.getParams(cluster, cmdRsp);
 
             if (!reqParams) return;
 
-            reqParams.forEach(function (arg) {
+            reqParams.forEach(function(arg) {
                 args[arg.name] = randomArg(arg.type);
             });
 
             funcObj = new FuncClass(cluster, 1, cmdRsp);
             payload = funcObj.frame(args);
 
-            funcObj.parse(payload, function (err, result) {
-                it(funcObj.cmd + ' frame() and parse() check', function () {
+            funcObj.parse(payload, function(err, result) {
+                it(funcObj.cmd + ' frame() and parse() check', function() {
                     expect(result).to.eql(args);
                 });
             });
@@ -88,9 +83,9 @@ describe('Functional CmdRsp framer and parser Check', function() {
 });
 
 function randomArg(type) {
-    var testBuf,
-        testArr,
-        k;
+    let testBuf;
+    let testArr;
+    let k;
 
     switch (type) {
         case 'uint8':
@@ -99,18 +94,18 @@ function randomArg(type) {
             return chance.integer({min: 0, max: 65535});
         case 'uint32':
             return chance.integer({min: 0, max: 4294967295});
-        case 'int8' :
+        case 'int8':
             return chance.integer({min: -128, max: 127});
-        case 'int16' :
+        case 'int16':
             return chance.integer({min: -32768, max: 32767});
-        case 'int32' :
+        case 'int32':
             return chance.integer({min: -2147483648, max: 2147483647});
         case 'floatle':
             return chance.floating({min: 0, max: 4294967295});
         case 'longaddr':
             return '0x00124b00019c2ee9';
         case 'stringPreLen':
-            var stringLen = chance.integer({min: 0, max: 255});
+            const stringLen = chance.integer({min: 0, max: 255});
             return chance.string({length: stringLen});
         case 'preLenUint8':
         case 'preLenUint16':
@@ -122,14 +117,15 @@ function randomArg(type) {
         case 'dynUint32':
             testArr = [];
             for (k = 0; k < 10; k += 1) {
-                if (type === 'dynUint8')
+                if (type === 'dynUint8') {
                     testArr[k] = chance.integer({min: 0, max: 255});
-                else if (type === 'dynUint16')
+                } else if (type === 'dynUint16') {
                     testArr[k] = chance.integer({min: 0, max: 65535});
-                else if (type === 'dynUint24')
+                } else if (type === 'dynUint24') {
                     testArr[k] = chance.integer({min: 0, max: 16777215});
-                else if (type === 'dynUint32')
+                } else if (type === 'dynUint32') {
                     testArr[k] = chance.integer({min: 0, max: 4294967295});
+                }
             }
             return testArr;
         case 'locationbuffer':
@@ -146,7 +142,10 @@ function randomArg(type) {
             }
             return testArr;
         case 'extfieldsets':
-            return [ { clstId: 0x0006, len: 0x3, extField: [0x01, 0x02, 0x03]}, { clstId: 0x0009, len: 0x5, extField: [0x05, 0x04, 0x03, 0x02, 0x01]} ];
+            return [
+                {clstId: 0x0006, len: 0x3, extField: [0x01, 0x02, 0x03]},
+                {clstId: 0x0009, len: 0x5, extField: [0x05, 0x04, 0x03, 0x02, 0x01]},
+            ];
         default:
             break;
     }
